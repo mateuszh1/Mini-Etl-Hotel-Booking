60edf121c6a19de00b59b1b7c4d49d018595fa2d
Mateusz <mateuszhorabik31@gmail.com>
2026-05-06 18:18:03 +0200

    Add basoc data quality checks

--- src/etl.py
+++ src/etl.py
@@ -11,3 +11,12 @@ print("CSV file loaded successfully!")
 print(f"Rows: {len(df)}")
 print(f"Columns: {len(df.columns)}")
 print(df.head())
+
+print("\n Missing values per column:")
+print(df.isnull().sum())
+
+print("\n Total missing values:")
+print(df.isnull().sum().sum())
+
+print("\n Total duplicate rows:")
+print(df.duplicated().sum())
